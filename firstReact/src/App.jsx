dbeb154ccfb96cd17a87c6d0312f11b8847f90cd
Claudia Salcedo <csprojectsinfo03@gmail.com>
2024-06-28 22:54:14 -0500

    first commit

---
+++ firstReact/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import NavBar from './components/NavBar'
+
+function App() {
+
+  return (
+    <>
+    <body className='body'>
+    <NavBar />
+    <div>
+     <p>Hello, world</p>
+    </div>
+    </body>
+    
+    <footer></footer>
+    </>
+  )
+}
+
+export default App
---
+++ firstReact/src/components/CartWidget.jsx
@@ -0,0 +1,14 @@
+import '../App.css'
+
+const CartWidget = () =>{
+return(
+
+    <div>
+        <img className='cart' src="../src/assets/shopping-cart.png" alt="" />
+        <span>0</span>
+    </div>
+
+)
+} 
+
+export default CartWidget
